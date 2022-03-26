24750cc905774d10e8a5dbb5ef24acf777d52c5f
christbordin <chrisbodin3151@gmail.com>
2022-03-27 00:12:46 +0700

    test added

---
+++ src/components/layouts/MyFooter.vue
@@ -0,0 +1,50 @@
+<template>
+    <footer class="footer">
+        <div class="columns is-variable is-1-mobile is-0-tablet is-3-desktop is-8-widescreen is-2-fullhd">
+          <div class="column">
+                <figure class="image is-128x128">
+                    <img src="../../assets/logo/UEFA.png">
+                </figure>
+          </div>
+          <div class="column">
+                <figure class="image is-128x128">
+                    <img src="../../assets/logo/premierleague.png">
+                </figure>
+          </div>
+          <div class="column">
+                <figure class="image is-128x128">
+                    <img src="../../assets/logo/laliga.png">
+                </figure>
+          </div>
+          <div class="column">
+                <figure class="image is-128x128">
+                    <img src="../../assets/logo/ligue1.png">
+                </figure>
+          </div>
+          <div class="column">
+                <figure class="image is-128x128">
+                    <img src="../../assets/logo/bundesliga.png">
+                </figure>
+          </div>
+          <div class="column">
+                <figure class="image is-128x128">
+                    <img src="../../assets/logo/seriea.png">
+                </figure>
+          </div>
+    </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'MyFooter',
+}
+</script>
+
+<style>
+.footer {
+    margin-top: 250px;
+}
+</style>
+
+    
